8261aafa0582f14b1412bedbef0842bad0610d6a
unknown <gaingademayur@gmail.com>
2022-06-22 22:16:51 +0530

    arrayList program updated

---
+++ arrayList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class arrayList {
+    public static void main(String[] args) {
+ArrayList<Integer> arr = new ArrayList<Integer>();  
+arr.add(10);
+arr.add(20);
+
+    System.out.println(arr);
+
+	}
+}
